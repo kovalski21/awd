333d6228209db6479dfa8d2cb7ac2b81e78bd275
mihail241006 <mihail.241006@gmail.com>
2024-06-16 15:15:36 +0300

    dxtkrdcyhdcjy

--- second_win.py
+++ second_win.py
@@ -1,1 +1,26 @@
 # напиши здесь код для второго экрана приложения
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout
+from instr import *
+class TestWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.connects()
+        self.show()
+    def set_appear():
+        self.
+    def initUI():
+        self.h_line = QHBoxLayout()
+        self.r_line = QVBoxLayout()
+        self.l_line = QVBoxLayout()
+        self.l_line.addWidget(self.btn1)
+        self.r_line.addWidget(self.timer)
+        self.h_line.addLayout(self.l_line)
+        self.h_line.addLayout(self.r_line)
+        self.setLayout(self.h_line)
+    def connects():
+        pass
+    def show():
+        pass    
