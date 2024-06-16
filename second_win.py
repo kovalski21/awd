ae11377641268be2a50ece96c49e75786705e86a
mihail241006 <mihail.241006@gmail.com>
2024-06-16 15:52:44 +0300

    [oihjnpoiuhjnp[

--- second_win.py
+++ second_win.py
@@ -9,18 +9,32 @@ class TestWin(QWidget):
         self.initUI()
         self.connects()
         self.show()
-    def set_appear():
-        self.
-    def initUI():
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width,win_height)
+        self.move(win_x,win_y)
+    def initUI(self):
+        self.btn_next = QPushButton(txt_sendresults)
+        self.btn1 = QPushButton(txt_test1)
+        self.btn2 = QPushButton(txt_test2)
+        self.btn3 = QPushButton(txt_test3)
+        self.timer = QLabel("0.00.00")
         self.h_line = QHBoxLayout()
         self.r_line = QVBoxLayout()
         self.l_line = QVBoxLayout()
         self.l_line.addWidget(self.btn1)
+        self.l_line.addWidget(self.btn2)
+        self.l_line.addWidget(self.btn3)
+        self.l_line.addWidget(self.btn_next)
         self.r_line.addWidget(self.timer)
         self.h_line.addLayout(self.l_line)
         self.h_line.addLayout(self.r_line)
         self.setLayout(self.h_line)
-    def connects():
-        pass
-    def show():
-        pass    
+    def connects(self):
+        self.btn_next.clicked.connect(self.next_click)       
+    def next_click(self):
+        self.hide()
+        #self.tw = FinalWin()
+app = QApplication([])
+mw = TestWin()
+app.exec_()
